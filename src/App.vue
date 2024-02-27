a459fc76bea9dfe016d9b2a147bb029ad7f16876
NikitaVelixJob <nikitavelix@yahoo.com>
2024-02-27 23:53:08 +0600

    new pic

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
     <div>
       <img src="/public/dima.jpg"/>
     </div>
-    <iframe frameborder="0" style="border:none;width:100%;height:210px;" width="100%" height="210" src="https://music.yandex.ru/iframe/track/16523469/5408809">Слушайте <a href='https://music.yandex.ru/album/5408809/track/16523469'>Habits (Stay High)</a> — <a href='https://music.yandex.ru/artist/1080505'>Tove Lo</a> на Яндекс Музыке</iframe>
+    <iframe frameborder="0" style="border:none;width:100%;height:210px;" width="500px" height="210" src="https://music.yandex.ru/iframe/track/16523469/5408809">Слушайте <a href='https://music.yandex.ru/album/5408809/track/16523469'>Habits (Stay High)</a> — <a href='https://music.yandex.ru/artist/1080505'>Tove Lo</a> на Яндекс Музыке</iframe>
 
   </div>
 </template>
@@ -27,6 +27,7 @@ img {
   align-items: center;
   flex-direction: column;
   border-radius: 14px;
+  max-width: 300px;
   height: fit-content;
   overflow: hidden;
 }
